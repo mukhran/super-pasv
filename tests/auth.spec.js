98996d5f0cf374bdf2665f2e3512d6fda3d891ae
Marina Mukhranski <mukhranski@gmail.com>
2021-07-14 15:29:15 -0700

    adding env variables (#1)
    * add api tests
    
    * env vars

---
+++ tests/auth.spec.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import supertest from 'supertest';
+
+
+
+
+describe('auth', function(){
+    const request = supertest(BASE_URL);
+    it('successful login', function(){
+        request
+            .post('/auth')
+            .send({login: LOGIN, password: PASSWORD })
+            .end(function(err, res){
+                expect(res.statusCode).to.eq(200);
+                expect(res.body.token).not.to.be.undefined;
+        });
+    });
+
+    it('log in with wrong credentials should return error', function() {
+        request
+            .post('/auth')
+            .send({login: 'wrong', password: 'wrong' })
+            .end(function(err,res){
+                expect(res.statusCode).to.eq(404);
+                expect(res.body.message).to.eq('Wrong login or password.');
+            });
+    })
+});
